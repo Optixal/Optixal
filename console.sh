ad923dad900f058cf009c1421fa01a27b2392c56
Shawn Pang <optixal@live.com.sg>
2021-11-05 04:09:41 +0800

    Create console.sh

---
+++ console.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Last login: $(date +'%a, %d %b %Y at %H:%M:%S %Z') from 10.1.33.7"
+echo
+echo '$ echo "hello world, im $(whoami)."'
+echo 'hello world, im shawn.'
+echo
+echo '$ ls interests'
+echo `ls interests`
+echo
+echo '$ head -1 /etc/hosts'
+echo '127.0.0.1 localhost 🇸🇬 singapore'
+echo
+echo '$ ^D'
