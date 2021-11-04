238fae5ddd53d1a0beede3fd2a754ea9f03c85ea
Shawn Pang <optixal@live.com.sg>
2021-11-05 04:50:09 +0800

    Update console.sh

--- console.sh
+++ console.sh
@@ -2,8 +2,8 @@
 
 echo "Last login: $(date +'%a, %d %b %Y at %H:%M:%S %Z') from 10.1.33.7"
 echo
-echo '$ echo "hello world, im $(whoami). this shell is live"'
-echo 'hello world, im shawn. this shell is live'
+echo '$ echo "hello world, im $(whoami)."'
+echo 'hello world, im shawn.'
 echo
 echo '$ ls ~/interests'
 echo `ls interests`
@@ -11,4 +11,4 @@ echo
 echo '$ head -1 /etc/hosts'
 echo '127.0.0.1 localhost 🇸🇬 singapore'
 echo
-echo '$ ^D'
+echo '$ # change is the only constant. ^U^D'
